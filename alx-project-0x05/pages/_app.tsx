3bbf4a2d51447b8d2e0204fb0afac8acf2c6099c
Salomegit <sallygithinji72@gmail.com>
2025-10-01 21:33:41 +0300

    refactor

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,4 +1,6 @@
-import Layout from "@/components/layouts/Layout";
+import Layout from 
+"@/components/layouts/Layout"
+;
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { CountProvider } from "@/context/CountContext";
